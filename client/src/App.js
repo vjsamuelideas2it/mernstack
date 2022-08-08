1b42080bc7c4bd8d095372989b1dc5c56f4619f4
Victor Jefferson <victorjefferson.samuel@ideas2it.com>
2022-08-08 16:32:43 +0530

    Added git view in profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
