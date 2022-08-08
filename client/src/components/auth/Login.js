98238aa72270833c6e2549d26348ffd84dba4e3e
Victor Jefferson <victorjefferson.samuel@ideas2it.com>
2022-08-08 17:01:38 +0530

    Realigned login and register page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -27,35 +27,38 @@ const Login = () => {
 
   return (
     <div className='container'>
-      <h1 className='large text-primary'>Sign In</h1>
-      <p className='lead'>Sign Into Your Account</p>
-      <form className='form' onSubmit={(e) => onSubmit(e)}>
-        <div className='form-group'>
-          <input
-            type='email'
-            placeholder='Email Address'
-            name='email'
-            value={email}
-            onChange={(e) => onChange(e)}
-            required
-          />
-        </div>
-        <div className='form-group'>
-          <input
-            type='password'
-            placeholder='Password'
-            autoComplete='true'
-            name='password'
-            value={password}
-            onChange={(e) => onChange(e)}
-            minLength='6'
-          />
-        </div>
-        <input type='submit' className='btn btn-primary' value='Login' />
-      </form>
-      <p className='my-1'>
-        Don't have an account? <Link to='/register'>Sign Up</Link>
-      </p>
+      <div className='auth-form-inner'>
+        <h1 className='large text-primary'>Sign In</h1>
+        <p className='lead'>Sign Into Your Account</p>
+        <form className='form' onSubmit={(e) => onSubmit(e)}>
+          <div className='form-group'>
+            <input
+              type='email'
+              placeholder='Email Address'
+              name='email'
+              value={email}
+              onChange={(e) => onChange(e)}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='auth-form'
+              placeholder='Password'
+              autoComplete='true'
+              name='password'
+              value={password}
+              onChange={(e) => onChange(e)}
+              minLength='6'
+            />
+          </div>
+          <input type='submit' className='btn btn-primary' value='Login' />
+        </form>
+        <p className='my-1'>
+          Don't have an account? <Link to='/register'>Sign Up</Link>
+        </p>
+      </div>
     </div>
   );
 };
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -36,60 +36,62 @@ const Register = () => {
 
   return (
     <div className='container'>
-      <h1 className='large text-primary'>Sign Up</h1>
-      <p className='lead'>Create Your Account</p>
-      <form className='form' onSubmit={(e) => onSubmit(e)}>
-        <div className='form-group'>
-          <input
-            type='text'
-            placeholder='Name'
-            name='name'
-            value={name}
-            onChange={(e) => onChange(e)}
-            required
-          />
-        </div>
-        <div className='form-group'>
-          <input
-            type='email'
-            placeholder='Email Address'
-            name='email'
-            value={email}
-            onChange={(e) => onChange(e)}
-            required
-          />
-          <small className='form-text'>
-            This site uses Gravatar so if you want a profile image, use a
-            Gravatar email
-          </small>
-        </div>
-        <div className='form-group'>
-          <input
-            type='password'
-            placeholder='Password'
-            autoComplete='true'
-            name='password'
-            value={password}
-            onChange={(e) => onChange(e)}
-            minLength='6'
-          />
-        </div>
-        <div className='form-group'>
-          <input
-            type='password'
-            placeholder='Confirm Password'
-            name='password2'
-            autoComplete='true'
-            value={password2}
-            onChange={(e) => onChange(e)}
-            minLength='6'
-          />
-        </div>
-        <input type='submit' className='btn btn-primary' value='Register' />
-      </form>
-      <p className='my-1'>
-        Already have an account? <Link to='/login'>Sign In</Link>
-      </p>
+      <div className='auth-form-inner'>
+        <h1 className='large text-primary'>Sign Up</h1>
+        <p className='lead'>Create Your Account</p>
+        <form className='form' onSubmit={(e) => onSubmit(e)}>
+          <div className='form-group'>
+            <input
+              type='text'
+              placeholder='Name'
+              name='name'
+              value={name}
+              onChange={(e) => onChange(e)}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='email'
+              placeholder='Email Address'
+              name='email'
+              value={email}
+              onChange={(e) => onChange(e)}
+              required
+            />
+            <small className='form-text'>
+              This site uses Gravatar so if you want a profile image, use a
+              Gravatar email
+            </small>
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Password'
+              autoComplete='true'
+              name='password'
+              value={password}
+              onChange={(e) => onChange(e)}
+              minLength='6'
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Confirm Password'
+              name='password2'
+              autoComplete='true'
+              value={password2}
+              onChange={(e) => onChange(e)}
+              minLength='6'
+            />
+          </div>
+          <input type='submit' className='btn btn-primary' value='Register' />
+        </form>
+        <p className='my-1'>
+          Already have an account? <Link to='/login'>Sign In</Link>
+        </p>
+      </div>
     </div>
   );
 };
