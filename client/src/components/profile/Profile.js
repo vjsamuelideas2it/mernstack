93e8e2acbae0655a72565a7ec5e7a81ef74cfeab
Victor Jefferson <victorjefferson.samuel@ideas2it.com>
2022-08-08 15:55:53 +0530

    Added exp and edu details in profile page

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getProfileById } from '../../actions/profile';
 import Spinner from '../layout/Spinner';
+import ProfileAbout from './ProfileAbout';
+import ProfileEducation from './ProfileEducation';
+import ProfileExperience from './ProfileExperience';
 import ProfileTop from './ProfileTop';
 // import Spinner from '../layout/Spinner';
 
@@ -29,6 +32,37 @@ const Profile = ({ match }) => {
           )}
           <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+            <div className='profile-exp bg-white p-2'>
+              <h2 className='text-primary'>Experience</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map((experience) => (
+                    <ProfileExperience
+                      key={experience._id}
+                      experience={experience}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No experience credentials</h4>
+              )}
+            </div>
+            <div className='profile-edu bg-white p-2'>
+              <h2 className='text-primary'>Education</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.education.map((education) => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education credentials</h4>
+              )}
+            </div>
           </div>
         </Fragment>
       )}
---
+++ client/src/components/profile/ProfileExperience.js
@@ -0,0 +1,25 @@
+import Moment from 'react-moment';
+
+const ProfileExperience = ({
+  experience: { company, title, location, current, to, from, description },
+}) => {
+  return (
+    <div>
+      <h3 className='text-dark'>{company}</h3>
+      <p>
+        <Moment format='DD/MM/YYYY'>{from}</Moment> -
+        {!to ? ' Now' : <Moment format='DD/MM/YYYY'>{to}</Moment>}
+      </p>
+      <p>
+        <strong>Position: </strong>
+        {title}
+      </p>
+      <p>
+        <strong>Description: </strong>
+        {description}
+      </p>
+    </div>
+  );
+};
+
+export default ProfileExperience;
