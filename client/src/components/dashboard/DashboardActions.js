f9d3c945c4b0be1116352879dd549af1a3276c48
Victor Jefferson <victorjefferson.samuel@ideas2it.com>
2022-08-12 14:14:31 +0530

    Design changes

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -1,6 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { deleteAccount } from '../../actions/profile';
+import profile from '../../assets/images/profile.png';
+import editProfile from '../../assets/images/edit-profile.png';
+import addExperience from '../../assets/images/add-experience.png';
+import addEducation from '../../assets/images/add-education.png';
+import deleteAccountIcon from '../../assets/images/delete-account.png';
 
 const DashboardActions = () => {
   const userId = useSelector((state) => state.profile.profile.user._id);
@@ -8,26 +13,67 @@ const DashboardActions = () => {
 
   return (
     <div className='dash-buttons-group'>
-      <Link to={`/profile/${userId}`} className='btn btn-dark'>
-        <i className='fas fa-user text-primary'></i> View Profile
-      </Link>
-      <Link to='/edit-profile' className='btn btn-dark'>
-        <i className='fas fa-edit text-primary'></i> Edit Profile
-      </Link>
-      <Link to='/add-experience' className='btn btn-dark'>
-        <i className='fab fa-black-tie text-primary'></i> Add Experience
-      </Link>
-      <Link to='/add-education' className='btn btn-dark'>
-        <i className='fas fa-graduation-cap text-primary'></i> Add Education
-      </Link>
-      <button
-        className='btn btn-danger'
-        onClick={() => {
-          dispatch(deleteAccount());
-        }}
-      >
-        <i className='fas fa-user-minus'></i> Delete My Account
-      </button>
+      <div className='btn btn-border'>
+        <Link className='d-flex align-items-center' to={`/profile/${userId}`}>
+          <img src={profile} alt='profile' className='dashboard-button' />
+          <span className='text-primary hide-md'>
+            <strong>View Profile</strong>
+          </span>
+        </Link>
+      </div>
+      <div className='btn btn-border'>
+        <Link className='d-flex align-items-center' to='/edit-profile'>
+          <img
+            src={editProfile}
+            alt='edit-profile'
+            className='dashboard-button'
+          />
+          <span className='text-primary hide-md'>
+            <strong>Edit Profile</strong>
+          </span>
+        </Link>
+      </div>
+      <div className='btn btn-border'>
+        <Link className='d-flex align-items-center' to='/add-experience'>
+          <img
+            src={addExperience}
+            alt='add-experience'
+            className='dashboard-button'
+          />
+          <span className='text-primary hide-md'>
+            <strong>Add Experience</strong>
+          </span>
+        </Link>
+      </div>
+      <div className=' btn btn-border'>
+        <Link className='d-flex align-items-center' to='/add-education'>
+          <img
+            src={addEducation}
+            alt='add-education'
+            className='dashboard-button'
+          />
+          <span className='text-primary hide-md'>
+            <strong>Add Eductaion</strong>
+          </span>
+        </Link>
+      </div>
+      <div className='btn btn-red btn-border-delete'>
+        <div
+          className='d-flex align-items-center'
+          onClick={() => {
+            dispatch(deleteAccount());
+          }}
+        >
+          <img
+            src={deleteAccountIcon}
+            alt='delete-account'
+            className='dashboard-button'
+          />
+          <span className='text-light hide-md'>
+            <strong>Delete Account</strong>
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
